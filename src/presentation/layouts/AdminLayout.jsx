50356edb10f9fca0fb8ddc25378e98c6a2faf39f
Galaxy-eyes2701 <khanhdong27012003@gmail.com>
2026-08-14 10:44:36 +0700

    fix một số css

--- src/presentation/layouts/AdminLayout.jsx
+++ src/presentation/layouts/AdminLayout.jsx
@@ -3,9 +3,9 @@ import AdminHeader from '@presentation/components/AdminHeader/AdminHeader';
 import Footer from '@presentation/components/Footer/Footer';
 
 const AdminLayout = () => (
-  <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+  <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', background: '#e0f2fe' }}>
     <AdminHeader />
-    <main style={{ flex: 1 }}>
+    <main style={{ flex: 1, background: '#e0f2fe' }}>
       <Outlet />
     </main>
     <Footer />
